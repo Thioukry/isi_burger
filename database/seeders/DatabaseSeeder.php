dd5e8cc27761a4c79c3652944761ab31eabc03ca
Thioukry <dieumbethioukry@gmail.com>
2026-04-10 00:26:05 +0000

    Gestion role client et admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,31 +13,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+
         $this->call([
             BurgerSeeder::class
         ]);
-
-        // User::factory()->create([
-        //  'name' => 'Test User',
-        //  'email' => 'test@example.com',
-        //]);
-        \App\Models\User::create([
+        User::create([
            'name' => 'Dieumbe Thioukry',
             'email' => 'dieumbethioukry@gmail.com',
-            'password' => bcrypt('password'),
-            // On utilise bcrypt pour hacher le mot de passe
+            'password' => bcrypt('2002'),
             'role' => 'admin',
         ]);
-        \App\Models\User::create([
-        'name' => 'Mbaye Ladiane',
+        User::create([
+            'name' => 'Mbaye Ladiane',
             'email' => 'mbayeladiane@gmail.com',
-            'password' => bcrypt('password'),
-            // On utilise bcrypt pour hacher le mot de passe
+            'password' => bcrypt('1234'),
             'role' => 'client',
-
-
         ]);
-
-        }
+    }
 }
